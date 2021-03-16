fc6b2d71f04dee3eb4403e1582819bc9b2f281c9
romatsypin <62996201+romatsypin@users.noreply.github.com>
2021-03-16 22:01:23 +0200

    v0.3

--- rating.py
+++ rating.py
@@ -1,4 +1,4 @@
 # Rating function
-def rate(rel_grades, irrel_grades):
-    a = 2 * rel_grades + 0.5 * irrel_grades
+def rate(rel_grades, irrel_grades, motivation):
+    a = 3 * rel_grades + irrel_grades + 0.5 * motivation
     return a
